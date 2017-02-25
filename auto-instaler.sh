00836299596b066dcafb808110c0a233290f8bbe
gabriellenhart <augustolenhart@gmail.com>
2017-02-25 02:34:36 -0300

    Alteração de arquivo. Inclusão de mais funções ao programa.

--- auto-instaler.sh
+++ auto-instaler.sh
@@ -8,6 +8,12 @@ echo "==========================================================
         2 - Codeblocks
         3 - Virtualbox
         4 - VLC player
+	5 - Chrome amd64
+	6 - Remover Chrome
+	7 - Oracle Java 8
+	8 - Remover Java 8
+	9 - Flash Palyer para firefox
+	10 - Remover Flash Player
         0 - Sair
 ==========================================================="
 echo -n "->OPÇÃO:  "
@@ -38,6 +44,39 @@ case $opcao in
         sudo add-apt-repository ppa:videolan/stable-daily
         sudo apt-get update
         sudo apt-get install vlc;;
+    
+    
+    5)  #Versão estável do repositório Google-Chorme amd64.
+	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+	#Chave de acesso ao repositório.
+	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install google-chrome-stable;;
+
+    6)	sudo apt-get remove google-chrome-stable;;
+	
+	#Java 8 e de derivados.
+    7)  sudo apt-get purge openjdk*
+	#Repositório atualizado da ultima versão.
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo apt-get update
+	sudo apt-get install oracle-java8-installer;;
+	#Ao executar o instalador, você terá que concordar com os termos de uso, para poder finalmente baixar e instalar o Java
+	#Se for necessário configurar mais alguma coisa, execute o comando: 
+	#sudo apt-get install oracle-java8-set-default.
+
+     8) sudo apt-get install ppa-purge -y #Remover Java.
+	sudo ppa-purge ppa:webupd8team/java
+	sudo apt-get update
+	sudo apt-get autoremove;;
+
+	#Repositório Adobe Flash Player para Firefox
+    9)	wget https://fpdownload.adobe.com/pub/flashplayer/pdc/24.0.0.194/flash_player_ppapi_linux.x86_64.tar.gz -O flash.tar.gz
+	tar -zxvf flash.tar.gz
+	sudo cp libflashplayer.so /usr/lib/mozilla/plugins/
+	sudo cp -r usr/* /usr
+	
+
     0)
         echo "Saindo..."
         exit;;
