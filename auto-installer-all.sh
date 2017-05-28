441fd8912f6a586a706fd8d7178ce598be4b89f9
Gabriellenhart <augustolenhart@gmail.com>
2017-05-28 15:52:03 -0300

    Alteração de nome de arquivo

---
+++ auto-installer-all.sh
@@ -0,0 +1,75 @@
+ear
+while true; do
+echo "==========================================================
+                Digite o número para realizar a função desejada:
+        1 - Instalar todos os programas úteis.
+	Xpdf
+	Vim
+	Vlc
+	Git
+	Links	
+	Youtube-dl
+	Virtualbox
+	Arduino
+	Aircrack-ng
+	Nmap
+	Htop
+	gcc
+	Weechat
+	Chrome amd64
+	Java 8
+	Teamviewer 10
+	
+
+        0 - Sair
+==========================================================="
+echo -n "->OPÇÃO:  "
+
+read opcao
+
+#verificar se foi digitada uma opcao
+if [ -z $opcao ]; then
+    echo "ERROOOOOOOU!!! Digita certo ai meu."
+    exit
+fi
+
+case $opcao in
+    1)
+	sudo apt-get update -y
+	sudo apt-get full-upgrade -y
+	sudo apt-get install Xpdf -y
+	sudo apt-get install vim -y
+	sudo apt-get install vlc -y
+	sudo apt-get install git -y
+	sudo apt-get install links -y
+	sudo apt-get install youtube-dl
+	sudo apt-get install virtualbox -y
+	sudo apt-get install redshift-gtk -y
+	sudo apt-get install arduino -y
+	sudo apt-get install aircrack-ng -y
+	sudo apt-get install nmap -y
+	sudo apt-get install htop -y
+	sudo apt-get install gcc -y
+	sudo apt-get install weechat -y
+	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' -y
+        wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	sudo apt-get update -y
+	sudo apt-get install google-chrome-stable -y
+	sudo add-apt-repository ppa:webupd8team/java -y
+	sudo apt-get install oracle-java8-installer -y
+        sudo dpkg --add-architecture i386 -y
+	sudo apt-get update -y
+        sudo apt-get install gdebi y
+        wget http://download.teamviewer.com/download/teamviewer_i386.deb -y
+        sudo gdebi teamviewer_linux.deb -y;;
+    0)
+echo
+echo "Saindo..."
+echo     exit;;
+    *)
+        echo
+        echo "ERROOOOOOOU!!!: Digita certo ai meu!"
+        echo ;;
+esac
+done
+
